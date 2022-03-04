f92b076aeb22343f1b22b0e5643201332cb2dab9
Deni Gentar Candana <76612860+denigentarcandana@users.noreply.github.com>
2022-03-04 22:26:36 +0700

    Add files via upload

---
+++ disablecopy.js
@@ -0,0 +1,35 @@
+// Matikan Salinan
+if (typeof document.onselectstart != "undefined") { document.onselectstart = new Function("return false"); } else { document.onmousedown = new Function("return false");
+  document.onmouseup = new Function("return false"); }
+
+
+// Notifikasi "Tidak Dapat Di Salin"
+
+var message = " PERINGATAN !\n Maaf, teks tersebut tidak dapat disalin.\n\nBila anda tertarik dengan script artikel kami, silahkan hubungi admin melalui media sosial Instagram kami.\n@zonber.official\n\n\n________________________________________\nAlamat Redaksi\n zonber.my.id\n\nJl. Raya Bayah-Cikotok No.Km. 01, Bayah Bar., Kec. Bayah, Kabupaten Lebak, Banten 42393, Indonesia\nEmail: zonber@gmail.com\nTelp: (0813) 1498 78 21\nFax: (0813) 1498 78 21\n\n";
+
+///////////////////////////////////
+function clickIE4() {
+  if (event.button == 2) {
+    alert(message);
+    return false;
+  }
+}
+
+function clickNS4(e) {
+  if (document.layers || document.getElementById && !document.all) {
+    if (e.which == 2 || e.which == 3) {
+      alert(message);
+      return false;
+    }
+  }
+}
+
+if (document.layers) {
+  document.captureEvents(Event.MOUSEDOWN);
+  document.onmousedown = clickNS4;
+}
+else if (document.all && !document.getElementById) {
+  document.onmousedown = clickIE4;
+}
+
+document.oncontextmenu = new Function("alert(message);return false")
