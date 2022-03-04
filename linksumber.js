8bb7488d228ee959442aa6613c5aea3c71bb49ed
Deni Gentar Candana <76612860+denigentarcandana@users.noreply.github.com>
2022-03-04 10:59:16 +0700

    Update linksumber.js

--- linksumber.js
+++ linksumber.js
@@ -1,5 +1,5 @@
 function nocopas() {
   var e = window.getSelection();
-  pagelink = "<br/><br/>Baca artikel zonber, selengkapnya" + document.location.href, copytext = e + pagelink, newdiv = document.createElement("div"), newdiv.style.position = "absolute", newdiv.style.left = "-99999px", document.body.appendChild(newdiv), newdiv.innerHTML = copytext, e.selectAllChildren(newdiv), window.setTimeout(function() { document.body.removeChild(newdiv) }, 100)
+  pagelink = " <br/><br/> Baca artikel zonber, selengkapnya " + document.location.href, copytext = e + pagelink, newdiv = document.createElement("div"), newdiv.style.position = "absolute", newdiv.style.left = "-99999px", document.body.appendChild(newdiv), newdiv.innerHTML = copytext, e.selectAllChildren(newdiv), window.setTimeout(function() { document.body.removeChild(newdiv) }, 100)
 }
 document.addEventListener("copy", nocopas);
