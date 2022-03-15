0ae3f2784faa583c65ac02a1567f817f2d9a248b
Deni Gentar Candana <76612860+denigentarcandana@users.noreply.github.com>
2022-03-16 02:05:42 +0700

    Update gentaropolis-main.js

--- gentaropolis-main.js
+++ gentaropolis-main.js
@@ -83,3 +83,27 @@ function setText() {
 
 setTimeout(setText, 1000);
 */
+
+
+//Network checker
+let online = document .getElementById("online");
+    let offline = document .getElementById("offline");
+
+     
+      let messageOffline = () => {
+      offline.style.display = "block";
+        online.style.display = "none";
+      };
+      
+let messageOnline = () => {
+online.style.display = "block";
+        offline.style.display = "none";
+      };
+      
+      if (window.navigator.onLine) {
+        messageOnline();
+      } else {
+        messageOffline();
+      }
+      window.addEventListener("online", messageOnline);
+      window.addEventListener("offline", messageOffline);
